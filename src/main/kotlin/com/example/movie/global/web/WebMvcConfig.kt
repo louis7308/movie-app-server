c7e582dd38746f77a0252e5792337b5874648914
전승원 <78971821+louis7308@users.noreply.github.com>
2023-05-31 11:51:18 +0900

    :sparkles: :: 5173 port origin 추가

--- src/main/kotlin/com/example/movie/global/web/WebMvcConfig.kt
+++ src/main/kotlin/com/example/movie/global/web/WebMvcConfig.kt
@@ -1,7 +1,6 @@
 package com.example.movie.global.web
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -12,6 +11,7 @@ class WebMvcConfig : WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedOrigins(
                 "http://localhost:3000",
+                "http://localhost:5173",
                 "https://gsm-movie.netlify.app"
             )
             .allowCredentials(true)
